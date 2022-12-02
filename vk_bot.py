7e99d52339558e1020bd8507423812d140618828
nikita <bigmama93@mail.ru>
2022-12-03 00:48:23 +0300

    added buttons

--- vk_bot.py
+++ vk_bot.py
@@ -1,6 +1,8 @@
 import random
 import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from vk_api.utils import get_random_id
 from environs import Env
 
 
@@ -8,12 +10,19 @@ env = Env()
 env.read_env()
 
 
+
+
 def echo(event, vk_api):
+    keyboard = VkKeyboard(one_time=True)
+    keyboard.add_button('Белая кнопка', color=VkKeyboardColor.POSITIVE)
     vk_api.messages.send(
         user_id=event.user_id,
         message=event.text,
-        random_id=random.randint(1, 1000)
+        random_id=random.randint(1, 1000),
+        keyboard=keyboard.get_keyboard(),
     )
+
+
 
 
 if __name__ == "__main__":
@@ -23,3 +32,4 @@ if __name__ == "__main__":
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
             echo(event, vk_api)
+            print(event.user_id)
